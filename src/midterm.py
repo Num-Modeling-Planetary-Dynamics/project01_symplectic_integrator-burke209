f9b66008edeef98824ef2423c910f23eaccf99ff
Angela Rose Burke <burke209@scholar-fe00.rcac.purdue.edu>
2022-10-26 12:43:47 -0400

    2nd commit

--- src/midterm.py
+++ src/midterm.py
@@ -11,10 +11,6 @@ import glob
 from astroquery.jplhorizons import Horizons
 import astropy.constants as const
 
-#kep_drift = drift_one (as in one body)
-
-#need heliocentric positions and barycentric velocities
-
 def get_vectors(obj_val):
     #obj_name should be an integer between 1-9
     #location as @sun gets heliocentric positions and velocities
@@ -24,41 +20,58 @@ def get_vectors(obj_val):
     vec_df = obj.vectors().to_pandas()
     el_df = obj.elements().to_pandas()
     
+    #units = AU
     rvecnames = ['x','y','z']
+    
+    #units = AU/d
     vvecnames = ['vx','vy','vz']
+    
+    #units = AU, unitless
     elnames = ['a','e']
     
     rvec = vec_df[rvecnames].iloc[0].to_numpy()
     vvec = vec_df[vvecnames].iloc[0].to_numpy()
     el = el_df[elnames].iloc[0].to_numpy()
     
-    obj_vec = {'rvec':rvec, 'vvec':vvec, 'el':el}
+    #convert AU to meter
+    rvec *= 1.496e+11
+    #convert AU/day to m/s
+    vvec *= 1.496e+11/86400
+    #convert AU to meter
+    el[0] *= 1.496e+11
     
-    #convert AUDay to AUYear
-    return(obj_vec)
+    obj_vec = {'rvec':rvec, 'vvec':vvec, 'el':el}
+
+    return obj_vec
+
+def get_step(a,mu):
+    P =2 * np.pi * np.sqrt(a**3/mu)
+    dt = P / 30.0
+    return dt
 
 #calculate heliocentric to barycentric velocity
 def vh2vb(vhvec, Gmass, Gmtot): 
     
     vbcb = -np.sum(Gmass * vhvec) / Gmtot
     vbvec = vhvec + vbcb
-    return(vbvec)
+    return vbvec
 
 #update heliocentric positions by barycentric velocity
-def lindrift(vbvec,rhvec,dt):
+def lindrift(vbvec,rhvec,Gmass,GMsun, dt):
     #vbvec = barycentric velocities
-    pt = np.sum(G * vbvec) / GMcb
+    pt = np.sum(Gmass * vbvec) / GMsun
     #rhvec is an nX3 array, lindrift is performed on all planets
     rhvec += pt *dt
-    return(rhvec)
+    return rhvec
 
 #calculate keplerian change in barycentric velocity and heliocentric velocity
-def kep_drift(mu,rvec_in,vvec_in,dt):
+def kep_drift(mu,rvec_in,vvec_in,a,ecc,dt):
     
     def danby(M,ecc):
         #initial guess
         k = 0.85
-        E = M +np.sign(np.sin(M)* k * ecc)
+        E = [M +np.sin(M)* k * ecc]
+        print(E)
         err_arr = [0.0]
         i=0
         while True :
@@ -71,25 +84,29 @@ def kep_drift(mu,rvec_in,vvec_in,dt):
             delta3 = 0.0-(f_E/(df_E+(0.5*delta2*df2_E)+((1./6.)*(delta2**2)*df3_E)))
             
             E_new =E[i] + delta3
-            err = (E_new-E[i])/(E_new) #use this less than 10^-12 instead of accuracy in deg
+            print(E_new)
+            err = (E_new-E[i])/E_new #use this less than 10^-12 instead of accuracy in deg
+            print(err)
             #acc = E_new-E[i]
-            E.append(E_new)
+            E = np.append(E,E_new)
             err_arr.append(err)
             i+=1
             
-            #if np.rad2deg(acc)<0.000001:
-             #   break
+            if err<10e-12:
+                break
         return(E[-1])
     
+    n = np.sqrt(mu / a**3)  # Kepler's 3rd law to get the mean motion
     rvec0 = rvec_in
-    vve0 = vvec_in
-    rmag0 = np.linalg.norm(rvec0)
-    #a,ecc = xv2el(mu,x,y,z,vx,vy,vz)
+    vvec0 = vvec_in
+    rmag0 = np.linalg.norm(rvec0,axis=1)
     E0 = np.arccos(-(rmag0-a)/(a * ecc))
+    print(E0)
     if np.sign(np.vdot(rvec0,vvec0)) < 0.0:
         E0 = 2*np.pi - E0
     M0 = E0 - ecc * np.sin(E0)
     M = M0 + n + dt
+    print(M)
     E = danby(M,ecc)
     dE = E-E0
     
@@ -100,28 +117,28 @@ def kep_drift(mu,rvec_in,vvec_in,dt):
     rvec = f * rvec0 +g *vvec0
     rmag = np.linalg.norm(rvec)
     
-    return()
+    fdot = -a**2 / (rmag * rmag0) * n * np.sin(dE)
+    gdot = a / rmag * (np.cos(dE) - 1.0) + 1.0
+
+    vvec = fdot * rvec0 + gdot * vvec0
+    
+    return rvec,vvec
 
 #update barycentric velocities
-def kick(rvec,vbvec,dt):
-    drvec = rvec - rvec[i,:]
-    irij3 = np.linalg.norm(drvec, axis=1)**3
-    irij3[i] = 1 #diagonal not included
-    irij3 = G / irij3
-    acc = np.sum(drvec.T * irij3)
+def kick(Gmass, rvec,vbvec,dt):
+    n = rvec.shape[0]
+    acc = []
+    Gmass_flat = Gmass.flatten()
+    for i in range(n):
+        drvec = rvec - rvec[i,:]
+        irij3 = np.linalg.norm(drvec, axis=1)**3
+        irij3[i] = 1 #diagonal not included
+        irij3 = Gmass_flat / irij3
+        acc.append([np.sum(drvec.T * irij3)])
     
     #calculate each ith body separately
     vbvec += acc * dt
-    return()
-
-def step(dt):
-    dth = 0.5 *dt
-    lindrift(dth)
-    kick(dth)
-    kep_drift(dt)
-    kick(dt)
-    lindrift(dth)
-    return()
+    return vbvec
     
 #------------------------------------------------------------------------------
 object_val_arr = [8,9]
@@ -134,9 +151,9 @@ a = []
 ecc = []
 mass = []
 
-G = np.longdouble(const.G.value)
+G = np.float64(const.G.value)
 AU2M = np.longdouble(const.au.value)
-Msun = np.longdouble(const.M_sun.value)
+Msun = np.float64(const.M_sun.value)
 Rsun = np.longdouble(const.R_sun.value)
 JD2S = 86400
 YR2S = np.longdouble(365.25 * JD2S)
@@ -146,36 +163,61 @@ YR2S = np.longdouble(365.25 * JD2S)
 #data processing
 for object_val in object_val_arr:
     object_vec[str(object_val)] = get_vectors(object_val)
-    object_vec[str(object_val)]['mass'] = object_mass[object_val]
+    object_vec[str(object_val)]['mass'] = object_mass[str(object_val)]
 
 for object_val in object_val_arr:
     rhvec.append(object_vec[str(object_val)]['rvec'])
     vhvec.append(object_vec[str(object_val)]['vvec'])
-    a.append(object_vec[str(object_val)]['el'][0])
+    a.append(np.float64(object_vec[str(object_val)]['el'][0]))
     ecc.append(object_vec[str(object_val)]['el'][1])
     mass.append(object_vec[str(object_val)]['mass'])
-    
+
+rhvec = np.array(rhvec)
+vhvec = np.array(vhvec)
+a = np.array(a)
+ecc = np.array(ecc)
+mass = np.array(mass)
+
 Gmass = G*(mass*10**24)
 Gmtot = G*Msun + np.sum(Gmass)
+mu = np.float64(G*(Msun+mass*10**24))
 
 #convert heliocentric velocity to barycentric velocity
 vbvec = vh2vb(vhvec,Gmass,Gmtot)
 
-dt = 
-for dt in timestep:
+#get step size for neptune
+dt = get_step(a[0],mu[0])
+dth = 0.5*dt
+tfinal = 1e5
+timestep = np.arange(0,tfinal,dt)
+t_arr = []
+rvec_arr = []
+vvec_arr = []
+
+for t in timestep:
     #drift heliocentric positions for each body by Sun
-
+    rhvec = lindrift(vbvec,rhvec,Gmass,G*Msun,dth)
+    print('Lin Drift 1 complete')
     #interaction kick for barycentric velocity
-
+    vbvec = kick(Gmass, rhvec,vbvec,dth)
+    print("Kick 1 complete")
     #keplerian drift heliocentric positions for each body
-
+    rhvec,vbvec = kep_drift(mu,rhvec,vbvec,a,ecc,dt)
+    print("Keplerian drift complete")
     #interaction kick for barycentric velocity
+    vbvec = kick(Gmass, rhvec,vbvec,dt)
+    print("Kick 2 complete")
+    #drift heliocentric positions for each body by Sun
+    rhvec = lindrift(vbvec,rhvec,Gmass,G*Msun,dth)
+    print("Lin Drift 2 complete")
+    #t_arr.append(t)
+    rvec_arr.append(rhvec)
+    vvec_arr.append(vbvec)
+    print(t)
     
-Mbody = object_mass[str(object_val)]
-mu = G * (Msun+Mbody)
-kick(dt)
-kep_drift(dt)
-kick(dt)
+
+    
+
     
 
 
